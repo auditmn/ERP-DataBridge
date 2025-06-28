086c1f808bc7597f68b4d405d6302786d53b16a6
Yash <yash.agrawal@st.niituniversity.in>
2025-06-28 17:03:36 +0530

    Removed Python Code

--- Uploader/components/logger_config.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-def setup_logging(name: str = __name__, level: int = logging.INFO):
-    logging.basicConfig(
-        level=level,
-        format='%(asctime)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S'
-    )
-    logging.getLogger(name).setLevel(level)
