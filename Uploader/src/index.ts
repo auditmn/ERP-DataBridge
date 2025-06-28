ed70a77fb5f14ab0ff472360d3dc0353757c1f0b
Yash <yash.agrawal@st.niituniversity.in>
2025-06-28 19:15:42 +0530

    Initialize

---
+++ Uploader/src/index.ts
@@ -0,0 +1,9 @@
+// src/index.ts
+const greeting: string = "Hello from your Node.js TypeScript project!";
+console.log(greeting);
+
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+console.log(`2 + 3 = ${add(2, 3)}`);
